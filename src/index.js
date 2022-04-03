50ff5aedb138a9c70d08604189e79d17be6ec674
shuhei <72839906+oleax21@users.noreply.github.com>
2022-04-03 18:05:39 +0900

    Connect Square and Board

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -28,7 +28,12 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
